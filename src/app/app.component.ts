5dad98790e31a7fc543614e9979cc76fe90d4983
luis-carvalho <carvalholuis@bitbucket.org>
2019-07-10 22:32:44 -0300

    firebase config, save user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+
+    firebase.initializeApp({
+      apiKey: "AIzaSyD5J3FjZG4YULjdwshZGgY2mcijOX7KOjg",
+      authDomain: "jta-instagram-clone-16763.firebaseapp.com",
+      databaseURL: "https://jta-instagram-clone-16763.firebaseio.com",
+      projectId: "jta-instagram-clone-16763",
+      storageBucket: "",
+      messagingSenderId: "270287201621",
+      appId: "1:270287201621:web:3db541515d9c14e9"
+    })
+  }
   title = 'instagram';
+
 }
--- src/app/services/authentication.services.ts
+++ src/app/services/authentication.services.ts
@@ -1,7 +1,24 @@
 import { Usuario } from '../model/usuario.model';
+import * as firebase from 'firebase'
 
 export class Authentication{
   public cadastrarUsuario(usuario: Usuario): void {
-    console.log(usuario)
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(usuario.email, usuario.senha)
+      .then((res: any) => {
+
+        delete usuario.senha;
+
+        firebase
+        .database()
+        .ref(`usuario_detalhe/${btoa(usuario.email)}`)
+        .set(usuario)
+      })
+      .catch((error: Error) => {
+        console.log(error)
+      })
+
   }
 }
