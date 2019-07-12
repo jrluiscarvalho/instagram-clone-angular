93253171bcd27d00d285443ba93219e95d963813
luis-carvalho <carvalholuis@bitbucket.org>
2019-07-12 12:40:34 -0300

    refact promise to order by newer

--- src/app/services/db.service.ts
+++ src/app/services/db.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase'
 import { ProgressService } from './progress.service';
+import { IncluirPublicacaoComponent } from '../components/home/incluir-publicacao/incluir-publicacao.component';
 
 @Injectable({
   providedIn: 'root'
@@ -45,35 +46,39 @@ export class DbService {
         firebase
         .database()
         .ref(`publicacoes/${btoa(email)}`)
+        .orderByKey()
         .once('value')
         .then((snapshot: any) => {
 
           let publications: Array<any> = [];
 
           snapshot.forEach((item: any) => {
-
             let publication = item.val()
+            publication.key = item.key
+            publications.push(publication)
+          });
+          return publications.reverse();
+        })
+        .then((publications: any) => {
 
+          publications.forEach(item => {
             firebase
             .storage()
             .ref()
             .child(`imagens/${item.key}`)
             .getDownloadURL()
             .then((url: string) => {
-
-              publication.url_image = url;
+              item.url_image = url;
 
               firebase
               .database()
               .ref(`usuario_detalhe/${btoa(email)}`)
               .once('value')
               .then((snapshot: any) => {
-                publication.nome_usuario = snapshot.val().nome_usuario;
-
-                publications.push(publication)
+                item.nome_usuario = snapshot.val().nome_usuario;
               })
             })
-          });
+          })
           resolve(publications)
         })
       });
